83b21a37987a2efc68f8d65d45b512b909f191d5
Cathal Horan <cathal.horan@intercom.io>
2017-02-10 06:00:34 -0500

    Changing PAT to AT for env variable. Can use either when running docker cmd but matching name change from personal access token to access token

--- users/users.rb
+++ users/users.rb
@@ -1,7 +1,7 @@
 require 'intercom'
 usr_id = ARGV[0]
 
-intercom = Intercom::Client.new(token: ENV['PAT'])
+intercom = Intercom::Client.new(token: ENV['AT'])
 
 user = intercom.users.find(:user_id => usr_id)
 puts(user.inspect)
